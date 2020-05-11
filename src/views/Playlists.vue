2760d5961949fc21811bd8936ab36a8e2eb1f123
akhilgoel0007 <akhilgoeljan@gmail.com>
2020-05-11 21:14:30 +0530

    Third Commit

--- src/views/Playlists.vue
+++ src/views/Playlists.vue
@@ -71,10 +71,9 @@
                       <v-list-item-title @click.stop="dialog = true">Add Tag</v-list-item-title>
                       <v-dialog v-model="dialog" max-width="290">
                         <v-card>
-                          <v-card-title class="headline">Use Google's location service?</v-card-title>
+                          <v-card-title class="headline">Add Tags</v-card-title>
                           <v-card-text>
-                            Let Google help apps determine location. This means sending anonymous location data to Google, even when no apps are running.
-                          </v-card-text>
+                         </v-card-text>
                           <v-card-actions>
                           <v-spacer></v-spacer>
                           <v-btn color="green darken-1" text @click="dialog = false">Save Changes</v-btn>
